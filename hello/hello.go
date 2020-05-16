606acc7988aa12315297f540184b0ac46112fd00
Arun Sivakumar <arunsivakumar@live.com>
2020-05-17 05:13:02 +0800

    add hello tests

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,11 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+//Hello this is a test
+func Hello(name string) string {
+	return "Hello, " + name
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("worlds"))
 }
